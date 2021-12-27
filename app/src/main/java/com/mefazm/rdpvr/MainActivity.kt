70f9e0642cfc663f53d8b72f1aa5307088b459b7
Oleksii Moskalenko <alfacentavra.a@gmail.com>
2021-12-28 00:35:43 +0200

    Add simple UI

--- app/src/main/java/com/mefazm/rdpvr/MainActivity.kt
+++ app/src/main/java/com/mefazm/rdpvr/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.mefazm.rdpvr
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "MainActivity")
+    }
+
+    fun getWindowSize() {
+        Log.i(TAG, "SDK API Level: ${Build.VERSION.SDK_INT}")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            TODO("Get Window size for 31 API.")
+        } else {
+            TODO("Get Window size for 29 API.")
+        }
     }
 }
