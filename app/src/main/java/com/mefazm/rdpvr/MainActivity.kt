f8da5dddc2a550e92dbfbf4c629b2dbbf6cdd6f8
Oleksii Moskalenko <alfacentavra.a@gmail.com>
2021-12-28 16:18:03 +0200

    Activate connect button

--- app/src/main/java/com/mefazm/rdpvr/MainActivity.kt
+++ app/src/main/java/com/mefazm/rdpvr/MainActivity.kt
@@ -3,6 +3,10 @@ package com.mefazm.rdpvr
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.Button
 import android.util.Log
 
 private const val TAG = "MainActivity"
@@ -12,6 +16,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i(TAG, "MainActivity")
+        val editTextHostname = findViewById<EditText>(R.id.editTextHostname)
+        val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
+        val editTextTextPassword = findViewById<EditText>(R.id.editTextTextPassword)
+        val buttonConnect = findViewById<Button>(R.id.buttonConnect)
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                buttonConnect.isEnabled = editTextHostname.text.isNotEmpty() && editTextUsername.text.isNotEmpty() && editTextTextPassword.text.isNotEmpty()
+            }
+        }
+        editTextHostname.addTextChangedListener(textWatcher)
+        editTextUsername.addTextChangedListener(textWatcher)
+        editTextTextPassword.addTextChangedListener(textWatcher)
     }
 
     fun getWindowSize() {
@@ -22,4 +41,6 @@ class MainActivity : AppCompatActivity() {
             TODO("Get Window size for 29 API.")
         }
     }
+
+
 }
