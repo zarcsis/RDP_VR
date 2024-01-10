6c27972451c97b26c68f06b4cd9e979890d2e348
Oleksii Moskalenko <alfacentavra.a@gmail.com>
2024-01-10 21:14:20 +0200

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 
     externalNativeBuild {
         cmake {
-            version = "3.28.0"
+            version = "3.28.1"
             path = file("src/main/cpp/CMakeLists.txt")
         }
     }
@@ -63,6 +63,6 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
